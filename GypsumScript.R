119ce812b64a7b5c8d56bbdda4c0788f9265d90d
nhaghdoost <haghdoost.nilou@yahoo.com>
2019-08-15 11:56:43 +0430

    data input

--- GypsumScript.R
+++ GypsumScript.R
@@ -3,4 +3,8 @@
 
 #### Gypsum soil Analysis
 
+# data input and managment
 
+gyps.data <- read.csv("GypsumMetaData.csv",header = T, row.names = 1)
+
+gyps.otu<- read.csv("GypsumOTUabundance.csv",header = T, row.names = 1)
